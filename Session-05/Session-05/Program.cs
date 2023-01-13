601e14f18015a8c18e398cd925cbddfbd9a830ca
Γεώργιος Κοσμαράς <giorgio2025@gmail.com>
2023-01-13 17:12:18 +0200

    all but No3 done

--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -2,6 +2,7 @@
 using Session_II;
 //using Session_III;
 using Session_IV;
+using Session_V;
 using System.Linq.Expressions;
 
 internal class Program
@@ -12,9 +13,12 @@ internal class Program
         SumProd temp1 = new SumProd();
         //PrimeFinder temp2 = new PrimeFinder();
         Arrays temp3 = new Arrays();
-        Console.WriteLine(temp.Answer());
-        Console.WriteLine(temp1.Answer());
+        Organiser temp4 = new Organiser();
+        Console.WriteLine(temp.Answer() + Environment.NewLine);
+        Console.WriteLine(temp1.Answer() + Environment.NewLine);
         temp3.Answer();
+        Console.WriteLine(Environment.NewLine);
+        temp4.Answer();
         Console.ReadLine();
     }
 }
---
+++ Session-05/Session-05/Task5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Session_V
+{
+    public class Organiser
+    {
+        public void Answer()
+        {
+            int[] array = { 0, 2, 1, 20, -31, 50, -4, 17, 89, 100 };
+            Array.Sort(array);
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
