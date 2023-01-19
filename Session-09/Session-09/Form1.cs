388ab43ceadbf79fa8e4d60852b7c07f524fa5e3
Γεώργιος Κοσμαράς <giorgio2025@gmail.com>
2023-01-19 15:35:07 +0200

    disallow invalid operator forms

--- Session-09/Session-09/Form1.cs
+++ Session-09/Session-09/Form1.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using System.Text.RegularExpressions;
 using System.Xml.Serialization;
 using Calculator_Library;
 
@@ -12,10 +13,21 @@ namespace Session_09
             InitializeComponent();
         }
 
-        public void buttonClick(object sender, EventArgs e)
+        private void buttonClick(object sender, EventArgs e)
         {
-            calculation += (sender as Button).Text;
-            textBox.Text = calculation;
+            string entry = (sender as Button).Text;
+            var regex = new Regex(@"(\d$)|([.]$)|(^$)");
+            if (regex.Match(calculation).Success)
+            {
+                calculation += entry;
+                textBox.Text = calculation;
+            }
+            else
+            {
+                calculation = calculation.Remove(calculation.Length - 1, 1);
+                calculation += entry;
+                textBox.Text = calculation;
+            }
         }
         public void buttonEqual(object sender, EventArgs e)
         {
