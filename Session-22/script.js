37335d49305094866ea594508368b29bf326a303
Γεώργιος Κοσμαράς <giorgio2025@gmail.com>
2023-02-07 16:22:01 +0200

    all but exc3 done

--- Session-22/script.js
+++ Session-22/script.js
@@ -5,13 +5,36 @@ function reverseStr()
     let result = temp.join("");
     document.getElementById("out-reverse").value = result;
 }
-function palindromise()
+function checkPalindrome()
 {
     let input = document.getElementById("pal-in").value;
     input = input.toLowerCase().replace(/[\W]/g, "");
     let temp = input.split("").reverse().join("");
-    document.getElementById("pal-out").value = (temp == input).toString();
+    if (temp == input)
+    {
+        document.getElementById("pal-out").value = "Yes";
+        document.getElementById("pal-out").style.backgroundColor="#90EE90"
+    }
+    else
+    {
+        document.getElementById("pal-out").value = "No";
+        document.getElementById("pal-out").style.backgroundColor="#FFCCCB"
+    }
 }
+
+function Inputs() {
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+    document.getElementById("output-multiply").value = multiply(a, b);
+
+}
+
+
+function multiply(a, b)
+{
+    return a*b;
+}
+
 
 function addToStr()
 {
