f2be2b32b00c476858bdb10d4e8c5a9203f5faf3
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-15 02:43:39 +0200

    Add PetFood DTOs

---
+++ Session-27/PetShop.Blazor/Shared/DTO/PetFood/PetFoodEditDto.cs
@@ -0,0 +1,14 @@
+﻿using PetShop.Models.Enums;
+
+namespace PetShop.Blazor.Shared.DTO.PetFood {
+
+    public class PetFoodEditDto {
+
+        public int Id { get; set; }
+        public AnimalType AnimalType { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+
+    }
+
+}
