23e895c21c3882f7d8d5c3066f82cf8ab94c99f9
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-15 02:27:01 +0200

    Add pet repo to program. Tests for pet: pass data to/from DB. All action working as expected (add, update, delete)

--- Session-27/PetShop.Blazor/Server/Program.cs
+++ Session-27/PetShop.Blazor/Server/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IEntityRepo<Customer>, CustomerRepo>();
 builder.Services.AddScoped<IEntityRepo<Employee>, EmployeeRepo>();
+builder.Services.AddScoped<IEntityRepo<Pet>, PetRepo>();
 
 var app = builder.Build();
 
