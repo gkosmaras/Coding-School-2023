dcfbf4ef9162386a8f32b2d45fb80dd929bbd628
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-15 04:35:20 +0200

    Add PetFood Controller

--- Session-27/PetShop.Blazor/Server/Controllers/PetFoodController.cs
+++ Session-27/PetShop.Blazor/Server/Controllers/PetFoodController.cs
@@ -1,20 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PetShop.EF.Repositories;
+using PetShop.Models;
+using PetShop.Models.Enums;
+using PetShop.Blazor.Shared.DTO.PetFood;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace PetShop.Blazor.Server.Controllers {
 
-namespace PetShop.Blazor.Server.Controllers
-{
-    public class PetFoodController : Controller
-    {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
+    [Route("[controller]")]
+    [ApiController]
+
+    public class PetFoodController : ControllerBase {
+
+        private readonly IEntityRepo<PetFood> _petFoodRepo;
+
+        public PetFoodController(IEntityRepo<PetFood> petFoodRepo) {
+            _petFoodRepo = petFoodRepo;
         }
-    }
-}
 
+        [HttpGet]
+        public async Task<IEnumerable<PetFoodDto>> Get() {
+
+            var response = _petFoodRepo.GetAll();
+
+            return response.Select(petFood => new PetFoodDto {
+                Id = petFood.Id,
+                AnimalType = petFood.AnimalType,
+                Price = petFood.Price,
+                Cost = petFood.Cost
+            });
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<PetFoodEditDto> GetById(int id) {
+
+            var response = _petFoodRepo.GetById(id);
+
+            return new PetFoodEditDto {
+                Id = id,
+                AnimalType = response.AnimalType,
+                Price = response.Price,
+                Cost = response.Cost
+            };
+
+        }
+
+        [HttpPost]
+        public async Task Post(PetFoodEditDto petFood) {
+            var newPetFood = new PetFood(petFood.AnimalType, petFood.Price, petFood.Cost);
+            _petFoodRepo.Add(newPetFood);
+        }
+
+        [HttpPut]
+        public async Task Put(PetFoodEditDto petFood) {
+
+            var itemToUpdate = _petFoodRepo.GetById(petFood.Id);
+
+            itemToUpdate.AnimalType = petFood.AnimalType;
+            itemToUpdate.Price = petFood.Price;
+            itemToUpdate.Cost = petFood.Cost;
+
+            _petFoodRepo.Update(petFood.Id, itemToUpdate);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id) {
+            _petFoodRepo.Delete(id);
+        }
+
+    }
+
+}
