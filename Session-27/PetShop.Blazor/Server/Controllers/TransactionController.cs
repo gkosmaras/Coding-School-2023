dbc76b3dcf2ca3d7a89e5847a0dfc84d8e798229
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-16 03:47:51 +0200

    Fix transaction bugs in edit screen. Add info columns to transaction table.

--- Session-27/PetShop.Blazor/Server/Controllers/TransactionController.cs
+++ Session-27/PetShop.Blazor/Server/Controllers/TransactionController.cs
@@ -2,30 +2,33 @@
 using Microsoft.AspNetCore.Mvc;
 using PetShop.EF.Repositories;
 using PetShop.Models;
-using PetShop.Models.Enums;
 using PetShop.Blazor.Shared.DTO.Transaction;
 using PetShop.Blazor.Shared.DTO.Customer;
 using PetShop.Blazor.Shared.DTO.Employee;
 using PetShop.Blazor.Shared.DTO.Pet;
 using PetShop.Blazor.Shared.DTO.PetFood;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 
-namespace PetShop.Blazor.Server.Controllers
-{
+namespace PetShop.Blazor.Server.Controllers {
 
     [Route("[controller]")]
     [ApiController]
 
-    public class TransactionController : ControllerBase
-    {
+    public class TransactionController : ControllerBase {
 
         private readonly IEntityRepo<Transaction> _transactionRepo;
         private readonly IEntityRepo<Customer> _customerRepo;
         private readonly IEntityRepo<Employee> _employeerRepo;
         private readonly IEntityRepo<Pet> _petRepo;
         private readonly IEntityRepo<PetFood> _petFoodRepo;
-        public TransactionController(IEntityRepo<Transaction> transactionRepo, IEntityRepo<Customer> customerRepo, IEntityRepo<Employee> employeerRepo, IEntityRepo<Pet> petRepo, IEntityRepo<PetFood> petFoodRepo)
-        {
+
+        public TransactionController(
+
+            IEntityRepo<Transaction> transactionRepo,
+            IEntityRepo<Customer> customerRepo,
+            IEntityRepo<Employee> employeerRepo,
+            IEntityRepo<Pet> petRepo,
+            IEntityRepo<PetFood> petFoodRepo) {
+
             _transactionRepo = transactionRepo;
             _customerRepo = customerRepo;
             _employeerRepo = employeerRepo;
@@ -34,13 +37,11 @@ namespace PetShop.Blazor.Server.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<TransactionDto>> Get()
-        {
+        public async Task<IEnumerable<TransactionDto>> Get() {
 
             var transactions = _transactionRepo.GetAll();
 
-            var result = transactions.Select(Transaction => new TransactionDto
-            {
+            var result = transactions.Select(Transaction => new TransactionDto {
                 Id = Transaction.Id,
                 Date = Transaction.Date,
                 PetPrice = Transaction.Pet.Price,
@@ -51,77 +52,101 @@ namespace PetShop.Blazor.Server.Controllers
                 EmployeeId = Transaction.EmployeeId,
                 PetId = Transaction.PetId,
                 PetFoodId = Transaction.PetFoodId,
+
+                CustomerName = Transaction.Customer.Name,
+                CustomerSurname = Transaction.Customer.Surname,
+                EmployeeName = Transaction.Employee.Name,
+                EmployeeSurname = Transaction.Employee.Surname,
+
+                PetBreed = Transaction.Pet.Breed,
+                PetFood = Transaction.PetFood.AnimalType
+
             });
+
             return result;
+
         }
+
         [HttpGet("{id}")]
-        public async Task <TransactionEditDto> GetById(int id)
-        {
+        public async Task<TransactionEditDto> GetById(int id) {
+
             var dbTransaction = _transactionRepo.GetById(id);
             var dbCustomer = _customerRepo.GetAll();
             var dbEmployee = _employeerRepo.GetAll();
             var dbPet = _petRepo.GetAll();
             var dbPetFood = _petFoodRepo.GetAll();
-            if (dbTransaction == null)
-            {
+
+            if (dbTransaction == null) {
                 throw new ArgumentNullException();
             }
-            var result = new TransactionEditDto
-            {
+
+            var result = new TransactionEditDto {
+
                 Date = DateTime.Now,
                 Id = dbTransaction.Id,
+                PetFoodQty = dbTransaction.PetFoodQty,
+
                 CustomerId = dbTransaction.CustomerId,
-                Customers = dbCustomer.Select(cus => new CustomerEditDto
-                {
-                    Id = cus.Id,
-                    Name = cus.Name,
+                Customers = dbCustomer.Select(customer => new CustomerEditDto {
+                    Id = customer.Id,
+                    //Name = customer.Name,
                 }).ToList(),
+
                 EmployeeId = dbTransaction.EmployeeId,
-                Employees = dbEmployee.Select(ee => new EmployeeEditDto
-                {
-                    Id = ee.Id,
-                    Name = ee.Name,
+                Employees = dbEmployee.Select(employee => new EmployeeEditDto {
+                    Id = employee.Id,
+                    // Name = employee.Name,
                 }).ToList(),
-                Pets = dbPet.Select(pet => new PetEditDto
-                {
+
+                PetId = dbTransaction.PetId,
+                Pets = dbPet.Select(pet => new PetEditDto {
                     Id = pet.Id,
-                    Breed = pet.Breed
+                    // Breed = pet.Breed
                 }).ToList(),
-                PetFoods = dbPetFood.Select(petFood => new PetFoodEditDto
-                {
+
+                PetFoodId = dbTransaction.PetFoodId,
+                PetFoods = dbPetFood.Select(petFood => new PetFoodEditDto {
                     Id = petFood.Id,
-                    AnimalType = petFood.AnimalType,
+                    // AnimalType = petFood.AnimalType,
                 }).ToList()
+
             };
+
             return result;
+
         }
 
         [HttpPost]
-        public async Task Post(TransactionEditDto transaction)
-        {
+        public async Task Post(TransactionEditDto transaction) {
+
             var pPrice = transaction.Pets.SingleOrDefault(x => x.Id == transaction.PetId).Price;
             var fQnt = transaction.PetFoodQty;
             var fPrice = (transaction.PetFoodQty) * transaction.PetFoods.SingleOrDefault(x => x.Id == transaction.PetFoodId).Price;
 
             var trans = new Transaction(pPrice, fQnt, fPrice, pPrice + fPrice);
+
             trans.CustomerId = transaction.CustomerId;
             trans.EmployeeId = transaction.EmployeeId;
             trans.PetId = transaction.PetId;
             trans.PetFoodId = transaction.PetFoodId;
+
             _transactionRepo.Add(trans);
+
         }
 
         [HttpPut]
-        public async Task Put(TransactionEditDto transaction)
-        {
+        public async Task Put(TransactionEditDto transaction) {
+
             var dbTransaction = _transactionRepo.GetById(transaction.Id);
-            if (dbTransaction == null)
-            {
+
+            if (dbTransaction == null) {
                 throw new ArgumentNullException();
             }
+
             var pPrice = transaction.Pets.SingleOrDefault(x => x.Id == transaction.PetId).Price;
             var fQnt = transaction.PetFoodQty;
             var fPrice = (transaction.PetFoodQty) * transaction.PetFoods.SingleOrDefault(x => x.Id == transaction.PetFoodId).Price;
+
             dbTransaction.PetPrice = pPrice;
             dbTransaction.PetFoodPrice = fPrice;
             dbTransaction.TotalPrice = pPrice + fPrice;
@@ -130,12 +155,13 @@ namespace PetShop.Blazor.Server.Controllers
             dbTransaction.EmployeeId = transaction.EmployeeId;
             dbTransaction.PetId = transaction.PetId;
             dbTransaction.PetFoodId = transaction.PetFoodId;
+
             _transactionRepo.Update(transaction.Id, dbTransaction);
+
         }
 
         [HttpDelete("{id}")]
-        public async Task Delete(int id)
-        {
+        public async Task Delete(int id) {
             _transactionRepo.Delete(id);
         }
     }
--- Session-27/PetShop.Blazor/Shared/DTO/Transaction/TransactionDto.cs
+++ Session-27/PetShop.Blazor/Shared/DTO/Transaction/TransactionDto.cs
@@ -3,6 +3,7 @@ using PetShop.Blazor.Shared.DTO.Employee;
 using PetShop.Blazor.Shared.DTO.Pet;
 using PetShop.Blazor.Shared.DTO.PetFood;
 using PetShop.Models;
+using PetShop.Models.Enums;
 
 namespace PetShop.Blazor.Shared.DTO.Transaction {
 
@@ -19,6 +20,14 @@ namespace PetShop.Blazor.Shared.DTO.Transaction {
         public int EmployeeId { get; set; }
         public int PetId { get; set; }
         public int PetFoodId { get; set; }
+
+        public string CustomerName { get; set; } = null!;
+        public string CustomerSurname { get; set; } = null!;
+        public string EmployeeName { get; set; } = null!;
+        public string EmployeeSurname { get; set; } = null!;
+        public string PetBreed { get; set; } = null!;
+        public AnimalType PetFood { get; set; }
+
         public List<CustomerDto> Customers { get; set; } = new();
         public List<EmployeeDto> Employees { get; set; } = new();
         public List<PetDto> Pets { get; set; } = new();
