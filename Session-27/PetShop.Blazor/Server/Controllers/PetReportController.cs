656febeed6873f7adb513e11f9ffe134ae10b5ae
Γεώργιος Κοσμαράς <giorgio2025@gmail.com>
2023-02-16 16:01:09 +0200

    attempt at report view

---
+++ Session-27/PetShop.Blazor/Server/Controllers/PetReportController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetShop.Blazor.Shared.DTO.Ledgers;
+using PetShop.EF.Repositories;
+using PetShop.Models;
+using PetShop.Models.Enums;
+using System.Globalization;
+
+namespace PetShop.Blazor.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PetReportController : ControllerBase
+    {
+        private readonly IEntityRepo<Transaction> _transactionRepo;
+        public PetReportController(IEntityRepo<Transaction> transactionRepo)
+        {
+            _transactionRepo = transactionRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<PetReportDto>> Get()
+        {
+            var dbTransactions = _transactionRepo.GetAll();
+            foreach (var date in dbTransactions)
+            {
+                date.Date = new DateTime(date.Date.Year, date.Date.Month, 1);
+            }
+            var animalTypes = Enum.GetNames(typeof(AnimalType)).ToList();
+/*                .Cast<AnimalType>()
+                .ToList();*/
+            var result = dbTransactions.GroupBy(x => x.Date)
+                .Select(ledge => new PetReportDto
+                {
+                    Year = ledge.First().Date.Year,
+                    Month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(ledge.First().Date.Month),
+                    AnimalTypes = animalTypes,
+                    TotalSold = 0
+                });
+            return result;
+        }
+
+    }
+}
---
+++ Session-27/PetShop.Blazor/Shared/DTO/Ledgers/PetReportDto.cs
@@ -0,0 +1,17 @@
+﻿using PetShop.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Blazor.Shared.DTO.Ledgers
+{
+    public class PetReportDto
+    {
+        public int Year { get; set; }
+        public string Month { get; set; } = null!;
+        public List<string> AnimalTypes { get; set; } = new();
+        public int TotalSold { get; set; }
+    }
+}
