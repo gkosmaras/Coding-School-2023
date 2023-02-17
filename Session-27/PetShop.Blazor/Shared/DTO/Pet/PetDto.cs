af87833e9c9d2d450aa1490f877df20de661ab2d
Γεώργιος Κοσμαράς <giorgio2025@gmail.com>
2023-02-17 05:10:19 +0200

    pet validation done

--- Session-27/PetShop.Blazor/Shared/DTO/Pet/PetDto.cs
+++ Session-27/PetShop.Blazor/Shared/DTO/Pet/PetDto.cs
@@ -1,10 +1,12 @@
 ﻿using PetShop.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace PetShop.Blazor.Shared.DTO.Pet {
 
     public class PetDto {
 
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter the pet's breed")]
         public string Breed { get; set; } = null!;
         public AnimalType AnimalType { get; set; }
         public PetStatus PetStatus { get; set; }
