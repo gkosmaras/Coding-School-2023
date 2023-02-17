591d8cc188e180841c086e45f58fcf23434b1ecc
Γεώργιος Κοσμαράς <giorgio2025@gmail.com>
2023-02-17 04:59:53 +0200

    customer validation done, minor changer in employee's

--- Session-27/PetShop.Blazor/Shared/DTO/Customer/CustomerEditDto.cs
+++ Session-27/PetShop.Blazor/Shared/DTO/Customer/CustomerEditDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,12 @@ namespace PetShop.Blazor.Shared.DTO.Customer
     public class CustomerEditDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = null!;
+        [Required]
         public string Surname { get; set; } = null!;
         public int Phone { get; set; }
+        [Required]
         public string Tin { get; set; } = null!;
         public List<TransactionDto> Transactions { get; set; } = new();
     }
