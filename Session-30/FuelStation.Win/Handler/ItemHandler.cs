76a59dc8e90b14e49f302d0817eb646544015975
Γεώργιος Κοσμαράς <giorgio2025@gmail.com>
2023-02-21 11:58:44 +0200

    started on item

--- Session-30/FuelStation.Win/Handler/ItemHandler.cs
+++ Session-30/FuelStation.Win/Handler/ItemHandler.cs
@@ -1,55 +1,49 @@
-﻿using Azure;
-using FuelStation.Web.Blazor.Shared.DTO;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions;
-using Newtonsoft.Json;
+﻿using FuelStation.Web.Blazor.Shared.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FuelStation.Win.Handler
 {
-    public class CustomerHandler
+    public class ItemHandler
     {
-        public async Task<IEnumerable<CustomerEditDto>> PopulateDataGridView()
+        public async Task<IEnumerable<ItemEditDto>> PopulateDataGridView()
         {
             HttpClient httpClient = new HttpClient();
             using (httpClient)
             {
-                var response = await httpClient.GetAsync("https://localhost:7209/customer");
-                var data = await response.Content.ReadAsAsync<IEnumerable<CustomerEditDto>>();
+                var response = await httpClient.GetAsync("https://localhost:7209/item");
+                var data = await response.Content.ReadAsAsync<IEnumerable<ItemEditDto>>();
                 return data;
             }
         }
 
-        public async Task AddCustomer(CustomerEditDto customer)
+        public async Task AddItem(ItemEditDto item)
         {
             HttpClient httpClient = new HttpClient();
-            var response = await httpClient.PostAsJsonAsync("https://localhost:7209/customer", customer);
+            var response = await httpClient.PostAsJsonAsync("https://localhost:7209/item", item);
             if (!response.IsSuccessStatusCode)
             {
                 MessageBox.Show("FailureAdd", "Error", MessageBoxButtons.OK);
             }
         }
 
-        public async Task EditCustomer(CustomerEditDto customer)
+        public async Task EditItem(ItemEditDto item)
         {
             HttpClient httpClient = new HttpClient();
-            var response = await httpClient.PutAsJsonAsync($"https://localhost:7209/customer", customer);
+            var response = await httpClient.PutAsJsonAsync($"https://localhost:7209/item", item);
             if (!response.IsSuccessStatusCode)
             {
                 MessageBox.Show("FailureEdit", "Error", MessageBoxButtons.OK);
             }
         }
 
-        public async Task  DeleteCustomer(int id)
+        public async Task DeleteItem(int id)
         {
             HttpClient httpClient = new HttpClient();
-            var response = await httpClient.DeleteAsync($"https://localhost:7209/customer/{id}");
+            var response = await httpClient.DeleteAsync($"https://localhost:7209/item/{id}");
             if (!response.IsSuccessStatusCode)
             {
                 MessageBox.Show("FailureDelete", "Error", MessageBoxButtons.OK);
